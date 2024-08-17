e892978c1664feaf76c20ba623522550086efda8
gelarahadian <gelarahadian38@gmail.com>
2024-08-17 13:45:42 +0700

    feat: add Header & defining Blog, Category, Feature, Footer, Hero, Subscribe Component

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="max-w-6xl mx-auto px-4">Footer</div>;
+};
+
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 inset-x-0 flex items-center h-20 ">
+      <div className="flex justify-between items-center w-full max-w-6xl px-4 mx-auto">
+        <h3 className="text-xl font-bold ">Red Plants</h3>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>Home</li>
+            <li>Catalogue</li>
+            <li>Contacts</li>
+          </ul>
+        </nav>
+        <div className="flex space-x-4">
+          <p>Search</p>
+          <p>Card(0)</p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
