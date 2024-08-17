be97ac940fb78239a2b22bddd5deb9490e651a8e
gelarahadian <gelarahadian38@gmail.com>
2024-08-17 15:01:17 +0700

    feat: add category contain

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,7 +1,52 @@
+import { Playfair_Display } from "next/font/google";
+import Image from "next/image";
+import { title } from "process";
 import React from "react";
 
+const playPairDisplay = Playfair_Display({ subsets: ["latin"] });
+
 const Category = () => {
-  return <div>Category</div>;
+  const categories = [
+    { title: "Jungle Plants", img: "/img/category-1.png" },
+    {
+      title: "Outdoor Plants",
+      img: "/img/category-2.png",
+    },
+    {
+      title: "Bedroom Plants",
+      img: "/img/category-3.png",
+    },
+    {
+      title: "Indoor Plants",
+      img: "/img/category-4.png",
+    },
+  ];
+  return (
+    <section>
+      <h3
+        className={`text-4xl text-center mb-16 ${playPairDisplay.className} `}
+      >
+        Categories
+      </h3>
+      <ul className="flex flex-wrap justify-between">
+        {categories.map((category) => (
+          <li key={category.title}>
+            <a href="#">
+              <Image
+                src={category.img}
+                alt={category.title}
+                width={260}
+                height={400}
+              />
+              <p className={`mt-5 text-2xl ${playPairDisplay.className}`}>
+                {category.title}
+              </p>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default Category;
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 inset-x-0 flex items-center h-20 ">
+    <header className="sticky top-0 inset-x-0 flex items-center h-20 bg-white/70 backdrop-blur">
       <div className="flex justify-between items-center w-full max-w-6xl px-4 mx-auto">
         <h3 className="text-xl font-bold ">Red Plants</h3>
         <nav>
